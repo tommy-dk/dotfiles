2ce969763bf54b95c5c0d6ed78afa102bc60ddc2
Tommy Heyde Olsen <tho@danskespil.dk>
2011-03-16 13:21:41 +0100

    Removed Mercurial plugin from vim Added Python skeleton file and extra python features in vimrc

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@
 
 if [ ! -n "$1" ] || [ ! -n "$2" ]
 then
-  echo -e "Usage: $(basename $0) install/remove <package>"
-  echo -e ""
-  echo -e "Available packages:"
-  echo -e "\tbash"
-  echo -e "\tvim"
-  echo -e "\tscreen"
-  echo -e "\tssh"
-  echo -e "\thg"
-  echo -e "\tmutt"
-  echo -e "\tirssi"
-  echo -e ""
+  echo "Usage: $(basename $0) install/remove <package>"
+  echo ""
+  echo "Available packages:"
+  echo "\tbash"
+  echo "\tvim"
+  echo "\tscreen"
+  echo "\tssh"
+  echo "\thg"
+  echo "\tmutt"
+  echo "\tirssi"
+  echo ""
   exit 1
 fi  
 
@@ -23,7 +23,7 @@ case "$1" in
   "install")
     case "$2" in
       "bash")
-        echo -e "Installing bash dotfiles"
+        echo "Installing bash dotfiles"
 	    ln -f bash/bash_profile ~/.bash_profile
 	    ln -f bash/bashrc ~/.bashrc
         ln -f bash/bash_aliases ~/.bash_aliases
@@ -31,14 +31,13 @@ case "$1" in
         ;;
 
       "vim")
-        echo -e "Installing vim dotfiles"
+        echo "Installing vim dotfiles"
         mkdir -p ~/.vim
         mkdir -p ~/.vim/backupfiles
         mkdir -p ~/.vim/plugin
         mkdir -p ~/.vim/tmp
         ln -f vim/vimrc ~/.vimrc
         ln -f vim/comments.vim ~/.vim/plugin/comments.vim
-        ln -f vim/hgcommand.vim ~/.vim/plugin/hgcommand.vim
         ln -f vim/openssl.vim ~/.vim/plugin/openssl.vim
         ln -f vim/skeleton.c ~/.vim/skeleton.c
         ln -f vim/skeleton.cc ~/.vim/skeleton.cc
@@ -46,26 +45,27 @@ case "$1" in
         ln -f vim/skeleton.h ~/.vim/skeleton.h
         ln -f vim/skeleton.html ~/.vim/skeleton.html
         ln -f vim/skeleton.tex ~/.vim/skeleton.tex
+        ln -f vim/skeleton.py ~/.vim/skeleton.py
         ;;
 
       "screen")
-        echo -e "Installing screen dotfiles"
+        echo "Installing screen dotfiles"
         ln -f screen/screenrc ~/.screenrc
         ;;
 
       "ssh")
-        echo -e "Installing vim dotfiles"
+        echo "Installing vim dotfiles"
         mkdir -p ~/.ssh
         ln -f ssh/config ~/.ssh/config
         ;;
 
       "hg")
-        echo -e "Installing hg dotfiles"
+        echo "Installing hg dotfiles"
         ln -f hg/hgrc ~/.hgrc
         ;;
 
       "mutt")
-        echo -e "Installing mutt dotfiles"
+        echo "Installing mutt dotfiles"
         ln -f mutt/muttrc ~/.muttrc
         mkdir -p ~/.mutt
         ln -f mutt/colors ~/.mutt/colors
@@ -76,14 +76,14 @@ case "$1" in
         ;;
 
       "irssi")
-        echo -e "Installing irssi dotfiles"
+        echo "Installing irssi dotfiles"
         mkdir -p ~/.irssi
         ln -f irssi/config ~/.irssi/config
         ln -f irssi/yotus.theme ~/.irssi/yotus.theme
         ;;
 
       *)
-        echo -e "No package with this name; exiting"
+        echo "No package with this name; exiting"
         exit 1
         ;;
       esac
@@ -92,49 +92,49 @@ case "$1" in
   "remove")
     case "$2" in
       "bash")
-        echo -e "Removing bash dotfiles"
+        echo "Removing bash dotfiles"
         rm -f ~/.bashrc ~/.bash_profile ~/.bash_aliases ~/.bash_atmail
         ;;
 
       "vim")
-        echo -e "Removing vim dotfiles"
+        echo "Removing vim dotfiles"
 	    rm -rf ~/.vimrc ~/.vim
         ;;
 
       "screen")
-        echo -e "Removing screen dotfiles"
+        echo "Removing screen dotfiles"
 	    rm -f ~/.screenrc
         ;;
 
       "ssh")
-        echo -e "Removing ssh dotfiles"
+        echo "Removing ssh dotfiles"
 	    rm -f ~/.ssh/config
         ;;
 
       "hg")
-        echo -e "Removing hg dotfiles"
+        echo "Removing hg dotfiles"
 	    rm -f ~/.hgrc
         ;;
 
       "mutt")
-        echo -e "Removing mutt dotfiles"
+        echo "Removing mutt dotfiles"
         rm -rf ~/.muttrc ~/.mutt ~/.msmtprc
         ;;
 
       "irssi")
-        echo -e "Removing irssi dotfiles"
+        echo "Removing irssi dotfiles"
         rm -f ~/.irssi/config ~/.irssi/yotus.theme
         ;;
 
       *)
-        echo -e "No package with this name; exiting"
+        echo "No package with this name; exiting"
         exit 1
       ;;
       esac
   ;;
 
   *)
-    echo -e "No action with this name; exiting"
+    echo "No action with this name; exiting"
     exit 1
   ;;
 
