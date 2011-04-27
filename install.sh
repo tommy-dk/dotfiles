d6198030506a261c713baa0473772d3a4e5d73ad
Tommy Heyde Olsen <tho@danskespil.dk>
2011-04-27 15:17:40 +0200

    Added 2 new plugins and some tweaks to these in the vimrc

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ case "$1" in
         ln -f vim/xoria256.vim ~/.vim/colors/xoria256.vim
         ln -f vim/pathogen.vim ~/.vim/autoload/pathogen.vim
         echo " - Downloading / Updating newest plugins from GitHub"
-        for plugin in nerdtree tcomment_vim vim-fugitive supertab vim-indent-guides vim-unimpaired
+        for plugin in nerdtree tcomment_vim vim-fugitive supertab vim-indent-guides vim-unimpaired gitv gundo.vim
         do
             if [ -d ~/.vim/bundle/"$plugin" ]; then
                 # if directory exists
@@ -83,6 +83,10 @@ case "$1" in
                         git clone https://github.com/nathanaelkane/vim-indent-guides.git -q;;
                     vim-unimpaired)
                         git clone https://github.com/tpope/vim-unimpaired.git -q;;
+                    gitv)
+                        git clone https://github.com/gregsexton/gitv.git -q;;
+                    gundo.vim)
+                        git clone http://github.com/sjl/gundo.vim.git -q;;
                     *)
                         # do nothing
                         ;;
